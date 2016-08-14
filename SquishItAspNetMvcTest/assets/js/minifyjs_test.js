82585a1df94cc0275b40276bd4982958a28a51db
Uong Dong Hung <hungud_x86@hotmail.com>
2016-08-14 20:50:56 +0700

    UPT CHECK

--- SquishItAspNetMvcTest/assets/js/minifyjs_test.js
+++ SquishItAspNetMvcTest/assets/js/minifyjs_test.js
@@ -1,3 +1,3 @@
 ﻿$(function () {
-    $("#js-test-container").html("This text was placed here by javascript. If you see this, the test completed successfully.");
+    $("#js-test-container").html("This text was placed here by javascript. If you see this, the test completed successfully. cached?");
 });
